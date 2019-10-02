26fc04234595135cf652fdcbcd0a8f636f1f6d8f
Iliya Zhechev <iliya.zhechev@gmail.com>
2019-10-03 01:39:54 +0300

    Implement print timer

--- pong/timer.py
+++ pong/timer.py
@@ -13,4 +13,14 @@ class Timer:
         return self.elapsed
 
 
+class PrintTimer(Timer):
+    def __init__(self, format='%.2fs'):
+        self.format = format
+
+    def __exit__(self, *exc_info):
+        super(PrintTimer, self).__exit__(None, None)
+        print(self.format % self.elapsed)
+
+
+print_timer = lambda *args: PrintTimer(*args)
 timer = Timer()
